6bcf174e5c18ba505170b55bc96ff30fe15f5f6d
Christophe Van de Poel <Christophe@roekeloos.be>
2020-02-08 13:28:57 +0100

    feat: ident rows with at least title width

--- src/components/ButtonRow/ButtonRow.vue
+++ src/components/ButtonRow/ButtonRow.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="tool__buttons--row">
     <Button @click.native="toggleActive" class="row-title">{{ title }}</Button>
-    <slot v-if="isExpanded" />
+    <div class="button-container">
+      <slot v-if="isExpanded" />
+    </div>
     <IconButton
       class="ButtonRow__expand"
       :icon="isExpanded ? 'minus' : 'plus'"
@@ -38,7 +40,7 @@ export default {
 .tool__buttons--row {
   font-size: 12px;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
 
   > * {
     margin-bottom: 3px;
@@ -63,11 +65,17 @@ export default {
 
 .row-title {
   background-color: #999999;
+  max-height: 14px;
 
   &:hover {
     background-color: #999999;
     }
   }
+.button-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
 
 /* alles wat specifiek voor de donkere versie is, zet je onderaan,
   tussen .dark-mode {} tags: (kopieer gewoon de stukken die je wilt veranderen)
