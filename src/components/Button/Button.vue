24b29d9468d6c1afc9e3ab108636309bb7cb3410
Christophe Van de Poel <Christophe@roekeloos.be>
2020-02-19 20:37:31 +0100

    disable select

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -28,6 +28,16 @@ export default {
   background-color: #ffffff;
   margin-bottom: 3px;
 
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
+  /* Non-prefixed version, currently
+    supported by Chrome, Opera and Firefox */
+
+
   &:hover {
     background-color: #cccccc;
   }
--- src/components/Slider/SliderButton.vue
+++ src/components/Slider/SliderButton.vue
@@ -182,6 +182,15 @@ export default {
 
 <style lang="scss" scoped>
 .SliderButton {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
+  /* Non-prefixed version, currently
+    supported by Chrome, Opera and Firefox */
+
   &.disable {
     opacity: 0.5;
     pointer-events: none;
